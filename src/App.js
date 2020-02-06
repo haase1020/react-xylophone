651e025b80d616bd8b6a908764af5f4e862e0d7f
Mandi Haase <haase1020@gmail.com>
2020-02-06 05:36:00 -0500

    add NoteButton component and props to that component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import notes from './assets/notes.js';
 import './App.scss';
 
+
+const NoteButton = (props) =>{
+  console.log("PROPS1", props)
+  return(
+    <button onClick={()=> new Audio(props.note.file).play()}>
+      {props.note.name}
+    </button>
+  )
+};
+//<Notebutton something="1" /> is the component name, then the props key name and value
 function App() {
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       < div className="xylophone">
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
-        <button onClick={(event)=> console.log("click")}>c</button>
+        <NoteButton note={notes[0]}/>
+        <button onClick={()=> new Audio(notes[0].file).play()}>c</button>
         <button>d1</button>
         <button>e1</button>
         <button>f</button>
