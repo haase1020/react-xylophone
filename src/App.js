b808e3431428b215ddfcfc65c407277c3f80fccb
Mandi Haase <haase1020@gmail.com>
2020-02-06 07:04:16 -0500

    add .note-button styling in App.scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import notes from './assets/notes.js';
 import './App.scss';
 
 
 const NoteButton = (props) =>{
   console.log("PROPS1", props)
+ 
+
   return(
-    <button onClick={()=> new Audio(props.note.file).play()}>
+    <button 
+    className="note-button"
+    onClick={()=> {
+      // props.playedNotes.push(props.note);
+      console.log("props.playedNotes",props.playedNotes);
+      props.setPlayedNotes([...props.playedNotes,props.note]);
+      new Audio(props.note.file).play();
+    }}>
       {props.note.name}
     </button>
   )
 };
 //<Notebutton something="1" /> is the component name, then the props key name and value
 function App() {
+  const [playedNotes, setPlayedNotes] = useState([]);
+
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       < div className="xylophone">
+        {JSON.stringify(playedNotes)}
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
-       {notes.map(noteOb => <NoteButton key={noteOb.name} note={noteOb} />)}
+       {notes.map(noteOb => (
+       <NoteButton 
+       playedNotes={playedNotes}
+       setPlayedNotes = {setPlayedNotes}
+       key={noteOb.name} 
+       note={noteOb} 
+       />
+       ))}
+
         
       </div>
       {/* <button>Replay</button> */}
